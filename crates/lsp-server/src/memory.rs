05e8e34376a1b1ceef7e6422e73d0f6677d5268a
davidsemakula <hello@davidsemakula.com>
2023-06-28 12:23:47 +0300

    update memory unit tests

--- crates/lsp-server/src/memory.rs
+++ crates/lsp-server/src/memory.rs
@@ -74,13 +74,13 @@ mod tests {
 
     #[test]
     fn memory_works() {
-        // Create memory instance.
+        // Creates memory instance.
         let mut memory = Memory::new();
 
         // Verifies has changes.
         assert!(!memory.has_changes());
 
-        // Add documents.
+        // Adds documents.
         memory.insert("1".to_string(), "A".to_string(), 0);
         memory.insert("2".to_string(), "B".to_string(), 0);
         memory.insert("3".to_string(), "C".to_string(), 0);
@@ -93,7 +93,7 @@ mod tests {
         );
         assert!(!memory.has_changes());
 
-        // Retrieve document.
+        // Retrieves document and verifies contents.
         assert_eq!(
             memory.get("1"),
             Some(&Document {
@@ -101,10 +101,11 @@ mod tests {
                 version: 0
             })
         );
+        // Tries to retrieve non-existent document.
         assert_eq!(memory.get("0"), None);
 
-        // Update document.
-        memory.update("1", "A1".to_string(), 1);
+        // Updates document and verifies updated contents.
+        assert!(memory.update("1", "A1".to_string(), 1));
         assert_eq!(
             memory.get("1"),
             Some(&Document {
@@ -112,15 +113,20 @@ mod tests {
                 version: 1
             })
         );
+        // Tries to update non-existent document and verifies that update doesn't perform an insert.
+        assert!(!memory.update("0", "Missing".to_string(), 1));
+        assert_eq!(memory.get("0"), None);
 
         // Verifies has and take changes.
         assert!(memory.has_changes());
         assert_eq!(memory.take_changes(), HashSet::from(["1".to_string()]));
         assert!(!memory.has_changes());
 
-        // Remove document.
-        memory.remove("1");
+        // Removes document and verifies memory after removal.
+        assert!(memory.remove("1").is_some());
         assert_eq!(memory.get("1"), None);
+        // Tries to remove non-existent document.
+        assert!(memory.remove("0").is_none());
 
         // Verifies has and take changes.
         assert!(memory.has_changes());
