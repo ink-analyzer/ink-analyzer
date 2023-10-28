7553eb36fba0b8d7345b5563cb60aa1be1941221
davidsemakula <hello@davidsemakula.com>
2023-10-28 20:51:49 +0300

    add integration tests for create project command

---
+++ crates/lsp-server/tests/create_project_command.rs
@@ -0,0 +1,97 @@
+//! integration tests for ink! Language Server create project command.
+
+mod utils;
+
+#[test]
+fn create_project_command_works() {
+    // Creates an in-memory connection to an initialized LSP server.
+    let client_connection = utils::create_initialized_lsp_server();
+
+    // Creates test project.
+    let project_name = "hello_ink";
+    let project_uri = lsp_types::Url::parse("file:///tmp/hello_ink/").unwrap();
+
+    // Creates LSP execute command request.
+    use lsp_types::request::Request;
+    let req_id = lsp_server::RequestId::from("create-project::hello_ink".to_string());
+    let req = lsp_server::Request {
+        id: req_id.clone(),
+        method: lsp_types::request::ExecuteCommand::METHOD.to_string(),
+        params: serde_json::to_value(&lsp_types::ExecuteCommandParams {
+            command: "createProject".to_string(),
+            arguments: vec![serde_json::json!({
+                "name": project_name,
+                "root": project_uri
+            })],
+            work_done_progress_params: Default::default(),
+        })
+        .unwrap(),
+    };
+    // Sends LSP execute command request from client to server.
+    client_connection.sender.send(req.into()).unwrap();
+
+    // Retrieves the LSP execute command response (from the server) on the client.
+    let resp = client_connection
+        .receiver
+        .iter()
+        .find_map(|message| match message {
+            lsp_server::Message::Response(it) => Some(it),
+            _ => None,
+        })
+        .unwrap();
+    // Verifies that the execute command response is for the current request
+    // and includes the expected results (i.e. successful but with an empty result).
+    assert_eq!(resp.id, req_id);
+    assert_eq!(resp.result, Some(serde_json::Value::Null));
+
+    // Retrieves the LSP `workspace/applyEdit` request (from the server) on the client.
+    let workspace_edit_req = client_connection
+        .receiver
+        .iter()
+        .find_map(|message| match message {
+            lsp_server::Message::Request(it) => {
+                (it.method == lsp_types::request::ApplyWorkspaceEdit::METHOD).then_some(it)
+            }
+            _ => None,
+        })
+        .unwrap();
+    // Verifies the expected workspace edit/ document changes.
+    let params: lsp_types::ApplyWorkspaceEditParams =
+        serde_json::from_value(workspace_edit_req.params).unwrap();
+    assert_eq!(params.label.as_deref(), Some("new ink! project"));
+    let document_changes = match params.edit.document_changes.unwrap() {
+        lsp_types::DocumentChanges::Edits(_) => None,
+        lsp_types::DocumentChanges::Operations(it) => Some(it),
+    }
+    .unwrap();
+    // Verifies that `lib.rs` and `Cargo.toml` files are created and contain expected content.
+    let lib_uri = project_uri.clone().join("lib.rs").unwrap();
+    let cargo_uri = project_uri.clone().join("Cargo.toml").unwrap();
+    let contains_file_create = |uri: &lsp_types::Url| {
+        document_changes.iter().any(|change| match change {
+            lsp_types::DocumentChangeOperation::Op(lsp_types::ResourceOp::Create(it)) => {
+                &it.uri == uri
+            }
+            _ => false,
+        })
+    };
+    let contains_file_content = |uri: &lsp_types::Url, pat: &str| {
+        document_changes.iter().any(|change| match change {
+            lsp_types::DocumentChangeOperation::Edit(it) => {
+                let edit_text = match &it.edits[0] {
+                    lsp_types::OneOf::Left(it) => &it.new_text,
+                    lsp_types::OneOf::Right(it) => &it.text_edit.new_text,
+                };
+                &it.text_document.uri == uri && edit_text.contains(pat)
+            }
+            _ => false,
+        })
+    };
+    assert!(contains_file_create(&lib_uri));
+    assert!(contains_file_create(&cargo_uri));
+    assert!(contains_file_content(
+        &lib_uri,
+        "#[ink::contract]\npub mod hello_ink {"
+    ));
+    assert!(contains_file_content(&cargo_uri, r#"name = "hello_ink""#));
+}
--- crates/lsp-server/tests/utils/mod.rs
+++ crates/lsp-server/tests/utils/mod.rs
@@ -2,7 +2,8 @@
 
 use std::thread;
 
-/// Creates an LSP server, starts its message dispatch loop and returns a in memory connection to it to simulate a connected client.
+/// Creates an LSP server, starts its message dispatch loop and
+/// returns a in memory connection to it to simulate a connected client.
 pub fn create_initialized_lsp_server() -> lsp_server::Connection {
     // Creates pair of in-memory connections to simulate an LSP client and server.
     let (server_connection, client_connection) = lsp_server::Connection::memory();
@@ -10,7 +11,8 @@ pub fn create_initialized_lsp_server() -> lsp_server::Connection {
     // Creates client capabilities.
     let client_capabilities = test_utils::simple_client_config();
 
-    // Runs the message dispatch loop on a separate thread (because `ink_lsp_server::main_loop` function is blocking).
+    // Runs the message dispatch loop on a separate thread
+    // (because `ink_lsp_server::main_loop` function is blocking).
     thread::spawn(|| ink_lsp_server::main_loop(server_connection, client_capabilities));
 
     // Returns client connection.
