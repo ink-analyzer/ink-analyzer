a4c2d27982a4f2db6cdf257ec35f42574e0350a3
davidsemakula <hello@davidsemakula.com>
2023-11-23 18:07:19 +0300

    restrict ast type trait bounds

--- crates/ir/src/traits/ast_type.rs
+++ crates/ir/src/traits/ast_type.rs
@@ -1,29 +1,30 @@
 //! ink! entity traits for accessing the wrapped AST type.
 
+use crate::InkEntity;
 use ra_ap_syntax::{ast, AstNode};
 
 use crate::tree::ast_ext;
 
 /// Implemented by ink! entities whose valid AST node is a `struct` item.
-pub trait IsInkStruct {
+pub trait IsInkStruct: InkEntity {
     /// Returns the `struct` item (if any).
     fn struct_item(&self) -> Option<&ast::Struct>;
 }
 
 /// Implemented by ink! entities whose valid AST node is an `fn` item.
-pub trait IsInkFn {
+pub trait IsInkFn: InkEntity {
     /// Returns the `fn` item (if any).
     fn fn_item(&self) -> Option<&ast::Fn>;
 }
 
 /// Implemented by ink! entities whose valid AST node is a `trait` item.
-pub trait IsInkTrait {
+pub trait IsInkTrait: InkEntity {
     /// Returns the `trait` item (if any).
     fn trait_item(&self) -> Option<&ast::Trait>;
 }
 
 /// Implemented by ink! entities whose valid AST parent item node is an `impl` item.
-pub trait HasInkImplParent {
+pub trait HasInkImplParent: InkEntity {
     /// Returns the `impl` item (if any) for the ink! entity's parent item node.
     fn parent_impl_item(&self) -> Option<ast::Impl>;
 }
